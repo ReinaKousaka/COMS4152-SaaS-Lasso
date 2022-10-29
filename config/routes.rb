deb11c380ada6ead23be27ea0adf8de1bfbaf41c
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-10-29 13:13:30 -0400

    reset template

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'movies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,8 +12,6 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
-  
   # Example resource route with options:
   #   resources :products do
   #     member do
