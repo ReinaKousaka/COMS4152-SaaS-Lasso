398fbae7e5e27afa817cb41b55bdb6fb0e89674e
kgl2123 <100443826+kgl2123@users.noreply.github.com>
2022-11-02 17:46:26 -0400

    added index

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -20,6 +20,14 @@ RSpec.describe EventsController, type: :controller do
                 :start_time => DateTime.parse('30th October 20:00:00'),
                 :end_time => DateTime.parse('30th October 22:00:00')
             })
+            Event.create({
+                :title => 'CS Coffee Chat', 
+                :category => "academic", 
+                :organizer => "CS Department", 
+                :location => "CS Lounge", 
+                :start_time => DateTime.parse('28th October 14:00:00'),
+                :end_time => DateTime.parse('28th October 16:00:00')
+            })
             @events = Event.all
         end
 
@@ -45,20 +53,32 @@ RSpec.describe EventsController, type: :controller do
             end
         end
 
-    describe 'deleting event' do 
-        it 'event that is deleted should not appear' do 
+        describe 'deleting event' do 
+            it 'event that is deleted should not appear' do 
 
-            event = @events.take
-            original_events_count = Event.all.count       
-            
-            delete :destroy, id: event.id
-            expect(Event.find_by_title("Films on Furnald: The Lion King")). to eq(nil)
-            expect(response).to redirect_to events_path
-            expect(@events.count).to eq(original_events_count - 1)
-            expect(flash[:notice]).to eq ("Event 'Films on Furnald: The Lion King' deleted.")
+                event = @events.take
+                original_events_count = Event.all.count       
+                
+                delete :destroy, id: event.id
+                expect(Event.find_by_title("Films on Furnald: The Lion King")). to eq(nil)
+                expect(response).to redirect_to events_path
+                expect(@events.count).to eq(original_events_count - 1)
+                expect(flash[:notice]).to eq ("Event 'Films on Furnald: The Lion King' deleted.")
+            end
         end
-    end
 
-    
-    end
+        describe 'index test' do 
+            it 'should return all events if no categories selected and no sort by' do 
+                get :index
+
+                expect(@categories_to_show_hash).to eq(nil)
+                expect(@sort_by).to eq(nil)
+                expect(session['categories']).to eq(nil)
+                expect(session['sort_by']).to eq(nil)
+                expect(@events.count).to eq (Event.all.count)
+                expect(@events.first).to eq(Event.first)
+            end
+        end
+        
+    end 
 end
--- spec/models/event_model_spec.rb
+++ spec/models/event_model_spec.rb
@@ -1,41 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
-	# context "Test basic DB methods" do
-    #     before :each do
-    #         Event.create(
-    #             :title => 'Films on Furnald: The Lion King', 
-    #             :category => "culture", :organizer => "Film Society", 
-    #             :location => "Furnald Lawn", 
-    #             :start_time => DateTime.parse('3rd Feb 2001 04:05:06')
-    #         )
-    #         Event.create(
-    #             :title => 'CS Coffee Chat', 
-    #             :category => "fun", :organizer => "CS department", 
-    #             :location => " Mudd"
-    #         )
-    #         @events = Event.all
-    #     end
-
-    #     it 'find event with a given title' do
-    #         event = Event.find_by(title: 'CS Coffee Chat')
-    #         expect(event.category).to eq('fun')
-    #     end
-    # end
-
-    # context 'Events page basic methods' do
-    #     before :each do
-    #         Event.delete_all
-    #         Event.create({
-    #             :title => 'Films on Furnald: The Lion King', 
-    #             :category => "culture", 
-    #             :organizer => "Film Society", 
-    #             :location => "Furnald Lawn", 
-    #             :start_time => DateTime.parse('30th October 20:00:00'),
-    #             :end_time => DateTime.parse('30th October 22:00:00')
-    #         })
-    #         @events = Event.all
-    #     end
 
     context 'Events model basics' do 
         before :each do 
