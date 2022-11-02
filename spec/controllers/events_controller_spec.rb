337cad24c588c2acaac18aab56fc1018fd618332
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-11-02 17:35:23 -0400

    Update events_controller_spec.rb

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe EventsController, type: :controller do
         end   
     end
 
-    # let!(:event) { create :events }
     context 'Events page basic methods' do
         before :each do
             Event.delete_all
@@ -20,6 +19,14 @@ RSpec.describe EventsController, type: :controller do
                 :start_time => DateTime.parse('30th October 20:00:00'),
                 :end_time => DateTime.parse('30th October 22:00:00')
             })
+            Event.create({
+                :title => 'CS Coffee Chat', 
+                :category => "academics", 
+                :organizer => "CS department", 
+                :location => " CS Lounge", 
+                :start_time => DateTime.parse('1st November 14:00:00'),
+                :end_time => DateTime.parse('1st November 15:00:00')
+            })
             @events = Event.all
         end
 
@@ -27,38 +34,42 @@ RSpec.describe EventsController, type: :controller do
             it 'Should be create a event' do
                 events_count = Event.all.count
                 event = {
-                    title: 'Varsity Football vs. UPenn', 
-                    category: 'athletics', 
-                    organizer: 'Varisty Football', 
-                    location: 'Baker Stadium', 
-                    start_time: DateTime.parse('13th November 15:00:00'),
-                    end_time: DateTime.parse('13th November 19:00:00')
+                    :title => 'Varsity Football vs. UPenn', 
+                    :category => 'athletics', 
+                    :organizer => 'Varisty Football', 
+                    :location => 'Baker Stadium', 
+                    :start_time => DateTime.parse('13th November 15:00:00'),
+                    :end_time => DateTime.parse('13th November 19:00:00')
                 }
 
-                # events_path events#create
                 get :create, event: event
-                
-                expect(event[:title]).to eq('Varsity Football vs. UPenn')
+
+                expect(flash[:notice]).to eq("Event '#{event[:title]}' was successfully created.")
                 expect(response).to redirect_to(events_path)
                 expect(@events.count).to eq(events_count + 1)
                 expect(flash[:notice]).to eq("Event '#{event[:title]}' was successfully created.")
             end
         end
 
-    describe 'deleting event' do 
-        it 'event that is deleted should not appear' do 
-
-            event = @events.take
-            original_events_count = Event.all.count       
-            
-            delete :destroy, id: event.id
-            expect(Event.find_by_title("Films on Furnald: The Lion King")). to eq(nil)
-            expect(response).to redirect_to events_path
-            expect(@events.count).to eq(original_events_count - 1)
-            expect(flash[:notice]).to eq ("Event 'Films on Furnald: The Lion King' deleted.")
+        describe 'deleting event' do 
+            it 'event that is deleted should not appear' do 
+                event = @events.take
+                original_events_count = Event.all.count       
+                
+                delete :destroy, id: event.id
+                expect(Event.find_by_title("Films on Furnald: The Lion King")). to eq(nil)
+                expect(response).to redirect_to events_path
+                expect(@events.count).to eq(original_events_count - 1)
+                expect(flash[:notice]).to eq ("Event 'Films on Furnald: The Lion King' deleted.")
+            end
         end
-    end
 
-    
+        it '.should render event with a given category' do
+            get :index, {
+                categories: {'academics' => 1}, 
+                button_clicked: true
+            }
+            expect(response).to render_template('index')
+        end
     end
 end
