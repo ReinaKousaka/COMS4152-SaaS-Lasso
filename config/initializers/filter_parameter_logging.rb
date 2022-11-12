ce4778ecdcd200522521f09a934d45031b52c6b5
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-11-12 17:16:52 -0500

    Update filter_parameter_logging.rb

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :authenticity_token]
+# https://guides.rubyonrails.org/configuring.html#rails-general-configuration
+# filter :authenticity_token out of logs
