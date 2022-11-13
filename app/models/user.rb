e154aa1a1f618570a641c60e052e156c1bd2e7bd
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-11-13 02:06:11 -0500

    Create Class ApplicationRecord

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates :email, presence: true
+  validates :username, uniqueness: true
+  validates :username, presence: true
+  # create bi-directional associations
+  # https://guides.rubyonrails.org/association_basics.html#bi-directional-associations
+  has_many :events
+
 end
