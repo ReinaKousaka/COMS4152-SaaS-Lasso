00c84adb472ec96a4fd7229e434f3de48a5075ed
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-11-15 23:26:17 -0500

    include Capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,6 @@ RSpec.configure do |config|
 	config.fixture_path = "#{::Rails.root}/spec/fixtures"
 	config.use_transactional_fixtures = true
 	config.infer_base_class_for_anonymous_controllers = false
+	# for capybara
+	config.include Capybara::DSL
 end
