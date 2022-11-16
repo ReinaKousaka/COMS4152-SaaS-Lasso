b0d79c33585b7f4376296253f738d37691d0f3b3
Yun-Yun Tsai <yt7821@columbia.edu>
2022-11-16 05:04:44 +0000

    fix bug!

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -5,26 +5,14 @@ Given /the following events exist/ do |event_table|
     end
 end
 
-<<<<<<< Updated upstream
-=======
-Given /the following users exist/ do |user_table|
-  user_table.hashes.each do |user|
-    User.create user
-  end
-end
+
   
->>>>>>> Stashed changes
 
 Then /(.*) seed events should exist/ do | n_seeds |
     expect(Event.count).to eq n_seeds.to_i
 end
 
-<<<<<<< Updated upstream
-=======
-Then /(.*) seed users should exist/ do | n_seeds |
-  expect(User.count).to eq n_seeds.to_i
-end
->>>>>>> Stashed changes
+
 
 Then /^(?:|I )should be on the edit page for "(.+)"$/ do |event_name|
   event_id = Event.find_by(title: event_name).id
