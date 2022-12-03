e90500b85333ae45921434b21bfcd4f346d8f861
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-12-03 17:25:56 -0500

    Fix account DNE error, add flash types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  add_flash_types :danger, :info, :warning, :success
+  
   def current_user
     if session[:user_id]
       User.find(session[:user_id])
