af8262cb111bd627e8c7378a83c79f8382366463
ReinaKousaka <songheng.yin@mail.utoronto.ca>
2022-12-17 00:00:00 -0500

    Update

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe UsersController, type: :controller do
         request.env['HTTP_REFERER'] = '/register'
     end
 
-    describe 'GET /register' do
+    describe 'GET users#new, /register' do
         it 'new' do
             get :new
             expect(response).to have_http_status(:success)
         end
     end
 
-    describe 'POST /users' do
+    describe 'POST users#create, /users' do
         it 'create new user successfully' do
             user = {
                 :email => 'user1@columbia.edu',
@@ -80,6 +80,25 @@ RSpec.describe UsersController, type: :controller do
             expect(User.all.count).to eq(1)
             expect(flash[:danger]).to eq('Email has already been taken')
             expect(response).to render_template('new')
+        end
+    end
+
+    describe 'GET users#show, /users/:id' do
+        let!(:event1) { FactoryGirl.create(:event) }
+        let!(:event2) { FactoryGirl.create(:event) }
+        let!(:user1) { FactoryGirl.create(:user) }
+        let!(:user2) { FactoryGirl.create(:user) }
+
+        it 'should display events belong to the user' do
+            get :show, id: user1.id
+            expect(response).to have_http_status(:success)
+            # TODO: check event1 and event2 are displayed here
+        end
+
+        it 'should not display other events' do
+            get :show, id: user2.id
+            expect(response).to have_http_status(:success)
+            # TODO: check no events is displayed here
         end
     end
 end
--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,16 +1,16 @@
 FactoryGirl.define do 
-    factory :event do
-        sequence(:title)      { |i| "Title#{i}" }
-        category              "fun"
-        location              "Mudd"
-        user_id               1
-        start_time            DateTime.parse('30th October 20:00:00')
-        end_time              DateTime.parse('30th October 22:00:00')
-    end
+  factory :event do
+      sequence(:title)      { |i| "Title#{i}" }
+      category              "fun"
+      location              "Mudd"
+      user_id               1
+      start_time            DateTime.parse('30th October 20:00:00')
+      end_time              DateTime.parse('30th October 22:00:00')
+  end
 
   factory :user do
-    email                 "user1@columbia.edu"
-    organizer_name        "organizer1"
+    sequence(:email)      { |i| "user#{i}@columbia.edu" }
+    sequence(:organizer_name)      { |i| "organizer#{i}" }
     password_digest       "password"
   end
 end
